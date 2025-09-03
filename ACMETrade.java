27f56f87918c867a6f8c103c68a81578cc742a17
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-09-03 10:34:55 -0300

    Feito os metodos 5, 6 , 7, 8 e 9!

--- ACMETrade.java
+++ ACMETrade.java
@@ -6,25 +6,21 @@ import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ACMETrade {
-    // Atributos para redirecionamento de E/S
-    private Scanner entrada = new Scanner(System.in);  // Atributo para entrada padrao (teclado)
-    private PrintStream saidaPadrao = System.out;   // Guarda a saida padrao - tela (console)
-    private final String nomeArquivoEntrada = "entrada.txt";  // Nome do arquivo de entrada de dados
-    private final String nomeArquivoSaida = "saida.txt";  // Nome do arquivo de saida de dados
+    private Scanner entrada = new Scanner(System.in);  
+    private PrintStream saidaPadrao = System.out;  
+    private final String nomeArquivoEntrada = "entrada.txt";  
+    private final String nomeArquivoSaida = "saida.txt";  
     private Federacao federacao;
     private Convencao convencao;
-
-    // Construtor da classe de aplicacao
     public ACMETrade() {
-        redirecionaIn();    // Redireciona Entrada para arquivos
-        redirecionaOut();    // Redireciona Saida para arquivos
+        redirecionaIn();    
+        redirecionaOut();   
         federacao = new Federacao();
         convencao = new Convencao();
     }
-
-
     public void executar(){
         while(true){
             String sigla = entrada.nextLine();
@@ -35,14 +31,24 @@ public class ACMETrade {
             String nome = entrada.nextLine();
             cadastrarPaises(sigla, nome);//metodo cadastrar paises
         }
-
+        //2
         cadastrarAcordos();
+        //3
         consultarPaisPelaSigla();
-
         //4:
-        int codigo = entrada.nextInt();
-        entrada.nextLine();
-        consultarAcordoPeloCodigo(codigo);
+        consultarAcordoPeloCodigo();
+        //5:
+        consultarAcordoPelaSiglaComp();
+        //6
+        mudarONomeDoPais();
+        //7:
+        removerAcordosPelaSiglaComp();
+        //8:
+        listarTodosOsAcordos();
+        //9:
+        listarNaoVendedores();
+
+
         
         
         // metodo consultar um pais pela sigla;
@@ -54,10 +60,9 @@ public class ACMETrade {
         // metodo lista todos os paises vendedores;
         // metodo mostrar o pais com a maior quantidade de acordos como vendedor;
     }
-
+//-------------------metodo-1------------------------------------------------------------------------------------------------
     public void cadastrarPaises(String sigla, String nome){
         boolean cadastrar = true;
-
         for(int i = 0; i < federacao.getPaises().size(); i++){
             if(federacao.getPaises().get(i).getSigla().equals(sigla)){
                 cadastrar = false;
@@ -70,9 +75,9 @@ public class ACMETrade {
             federacao.getPaises().add(p);
             System.out.println("1:" + sigla + ";"+ nome);
         }
-
     }
-
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------metodo-2------------------------------------------------------------------------------------------------
     public void cadastrarAcordos(){
         while(true){
             int codigo = entrada.nextInt();
@@ -88,8 +93,6 @@ public class ACMETrade {
             String vend = entrada.nextLine();
             Pais comprador = verificaSeExistePaisComprador(comp);
             Pais vendedor = verificaSeExistePaisVendedor(vend);
-            //metodo de cadastrar acordos
-
             if(verificar && comprador != null && vendedor != null){
                 Acordo acordo = new Acordo(codigo, produto, taxa, comprador, vendedor);
                 convencao.getAcordo().add(acordo);
@@ -102,9 +105,7 @@ public class ACMETrade {
                 System.out.println("2:erro-vendedor inexistente.");
             }
         }
-
     }
-
     public boolean verificaSeCodigoJaExiste(int codigo){
         boolean verificar = true;
         for(int i = 0; i<convencao.getAcordo().size(); i++){
@@ -115,7 +116,6 @@ public class ACMETrade {
         }
         return verificar;
     }
-
     public Pais verificaSeExistePaisComprador(String c){
         String comprador = null;
         Pais verifica = null;
@@ -128,7 +128,6 @@ public class ACMETrade {
         }
         return verifica;
     }
-
     public Pais verificaSeExistePaisVendedor(String v){
         String vendedor = null;
         Pais verificar = null;
@@ -140,7 +139,8 @@ public class ACMETrade {
         }
         return verificar;
     }
-
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------metodo-3----------------------------------------------------------------------------------------------
     public void consultarPaisPelaSigla(){
         String sigla = entrada.nextLine();
         Pais pais = null; 
@@ -155,8 +155,11 @@ public class ACMETrade {
             System.out.println("3:erro-sigla inexistente.");
         }
     }
-
-    public void consultarAcordoPeloCodigo(int codigo) {
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------metodo-4----------------------------------------------------------------------------------------------
+    public void consultarAcordoPeloCodigo() {
+        int codigo = entrada.nextInt();
+        entrada.nextLine();
         Acordo a = null;
         for(int i = 0; i < convencao.getAcordo().size(); i++) {
             a = convencao.getAcordo().get(i);
@@ -169,47 +172,125 @@ public class ACMETrade {
             System.out.println("4:erro-codigo inexistente.");
         }
     }
+//---------------------------------------------------------------------------------------------------------------------------
+//--------------------metodo-5-----------------------------------------------------------------------------------------------
+    public void consultarAcordoPelaSiglaComp() {
+        String sigla = entrada.nextLine();
+        Acordo a = null;
+        for(int i = 0; i < convencao.getAcordo().size(); i++) {
+            if(convencao.getAcordo().get(i).getComprador().getSigla().equals(sigla)) {
+                a = convencao.getAcordo().get(i);
+                System.out.println("5:" + a.getCodigo() + ";" + a.getProduto() + ";" + a.getTaxa() + ";" + a.getVendedor().getSigla() + ";" + a.getComprador().getSigla());
+            }
+        }
+        if(a == null) {
+            System.out.println("5:erro-sigla inexistente.");
+        }
+    }
+//---------------------------------------------------------------------------------------------------------------------------
+//-----------------metodo-6--------------------------------------------------------------------------------------------------
+    public void mudarONomeDoPais(){
+        String sigla = entrada.nextLine();
+        String name = entrada.nextLine();
+        Pais pais = null;      
+        String nome = null;  
+        for(int i = 0; i<federacao.getPaises().size(); i++){
+            if(federacao.getPaises().get(i).getSigla().equals(sigla)){
+                pais = federacao.getPaises().get(i);
+                nome = pais.getNome();
+                pais.setNome(nome);
+                System.out.println("6:" + pais.getSigla() + ";" + name);
+            }if(pais == null){
+                System.out.println("6:erro-sigla inexistente.");
+            }
+        }
+    }
+//---------------------------------------------------------------------------------------------------------------------------
+//-----------------metodo-7--------------------------------------------------------------------------------------------------
+    public void removerAcordosPelaSiglaComp() {
+        String sigla = entrada.nextLine();
+        boolean verify = false;
+        Acordo a = null;
+        for(int i = 0; i < convencao.getAcordo().size(); i++) {
+            a = convencao.getAcordo().get(i);
+            if(a.getComprador().getSigla().equals(sigla)) {
+                convencao.getAcordo().remove(i);
+                verify = true;
+            }
+        }
+        if(verify) {
+            System.out.println("7:acordos removidos.");
+        } else {
+            System.out.println("7:erro-nenhum acordo encontrado");
+        }
+    }
+//---------------------------------------------------------------------------------------------------------------------------
+//-----------------metodo-8--------------------------------------------------------------------------------------------------
+    public void listarTodosOsAcordos(){
+        Acordo acordo = null;
+        if(convencao.getAcordo().size() > 0){
+            for(int i = 0; i <= convencao.getAcordo().size(); i++){
+                acordo = convencao.getAcordo().get(i);
+                System.out.println("8:" + acordo.getCodigo() + ";" + acordo.getProduto() + ";" + acordo.getTaxa() + ";" + acordo.getComprador().getSigla() + ";" + acordo.getVendedor().getSigla());                    
+            }
+        }else{
+            System.out.println("8:erro-nenhum acordo cadastrado.");
+        }
+        
+    }
+//---------------------------------------------------------------------------------------------------------------------------
+//-----------------metodo-9--------------------------------------------------------------------------------------------------
+    public void listarNaoVendedores(){
+        ArrayList<Pais> paises = new ArrayList<Pais>();
+        Pais c = null;
+        for(int i = 0; i < federacao.getPaises().size(); i++) {
+            paises.add(federacao.getPaises().get(i));
+        }
+        for(int i = 0; i < paises.size(); i++) {
+            for(int j = 0; j < convencao.getAcordo().size(); j++) {
+                if(paises.get(i).getSigla().equals(convencao.getAcordo().get(j).getVendedor().getSigla())) {
+                    paises.remove(i);
+                }
+            }
+        }
+        if(paises.size() == 0) {
+            System.out.println("9:erro-nenhum país encontrado.");
+        } else {
+            for(int k = 0; k < paises.size(); k++) {
+                System.out.println("9:" + paises.get(k).getSigla() + ";" + paises.get(k).getNome());
+            }
+        }
+    }
+//---------------------------------------------------------------------------------------------------------------------------
+//-----------------metodo-10--------------------------------------------------------------------------------------------------
+    public void metodo10() {
 
-    
-
-
-    // Redireciona Entrada de dados para arquivos em vez de teclado
-    // Chame este metodo para redirecionar a leitura de dados para arquivos
+    }
+//---------------------------------------------------------------------------------------------------------------------------
     private void redirecionaIn() {
         try {
             BufferedReader streamEntrada = new BufferedReader(new FileReader(nomeArquivoEntrada));
-            entrada = new Scanner(streamEntrada);   // Usa como entrada um arquivo
+            entrada = new Scanner(streamEntrada);   
         } catch (Exception e) {
             System.out.println(e);
         }
-        Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
-        entrada.useLocale(Locale.ENGLISH);   // Ajusta para leitura para ponto decimal
+        Locale.setDefault(Locale.ENGLISH);   
+        entrada.useLocale(Locale.ENGLISH);  
     }
-
-    // Redireciona Saida de dados para arquivos em vez da tela (terminal)
-    // Chame este metodo para redirecionar a escrita de dados para arquivos
     private void redirecionaOut() {
         try {
             PrintStream streamSaida = new PrintStream(new File(nomeArquivoSaida), Charset.forName("UTF-8"));
-            System.setOut(streamSaida);             // Usa como saida um arquivo
+            System.setOut(streamSaida);             
         } catch (Exception e) {
             System.out.println(e);
         }
-        Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
+        Locale.setDefault(Locale.ENGLISH); 
     }
-
-    // Restaura Entrada padrao para o teclado
-    // Chame este metodo para retornar a leitura de dados para o teclado
+    
     private void restauraEntrada() {
         entrada = new Scanner(System.in);
     }
-
-    // Restaura Saida padrao para a tela (terminal)
-    // Chame este metodo para retornar a escrita de dados para a tela
     private void restauraSaida() {
         System.setOut(saidaPadrao);
     }
-
-    
-
 }
